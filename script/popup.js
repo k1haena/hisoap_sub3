d7fb1a00e1d7a7e94f25b3ef7fcf5f32866c20be
k1minseong <99226683+k1minseong@users.noreply.github.com>
2022-02-22 13:10:00 +0900

    Add files via upload

---
+++ script/popup.js
@@ -0,0 +1,8 @@
+$(function(){
+    $(".popup1 a").click(function(){
+        $(".pop1").show();//.fadeIn(); 서서히 나타남.
+    });
+    $(".pop1 a").click(function(){
+        $(".pop1").hide();//.fadeOut(); 서서히 사라짐.
+    });
+});
